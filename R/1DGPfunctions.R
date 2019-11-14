2699a0ce33d121de495051cfe5805463c18fd5ed
lmmontoya <lmontoya@berkeley.edu>
2019-11-13 20:05:43 -0500

    add DGP with obv int

--- R/1DGPfunctions.R
+++ R/1DGPfunctions.R
@@ -1,5 +1,5 @@
-#' @name QAW_AL_bin
-#' @aliases QAW_AL_bin
+#' @name QAW_bin
+#' @aliases QAW_bin
 #' @title Simulate with AL bin DGP
 #' @description Generate QAW according to AL bin DGP
 #'
@@ -16,7 +16,7 @@
 ### AL DGP binary outcome #####################
 ###############################################
 # QAW
-QAW_AL_bin = function(A, W) {
+QAW_bin = function(A, W) {
 
   W1 = W$W1
   W2 = W$W2
@@ -29,8 +29,8 @@ QAW_AL_bin = function(A, W) {
 
 
 
-#' @name DGP_AL_bin
-#' @aliases DGP_AL_bin
+#' @name DGP_bin
+#' @aliases DGP_bin
 #' @title Simulate with AL bin DGP
 #' @description Generate data according to AL bin DGP
 #'
@@ -44,7 +44,7 @@ QAW_AL_bin = function(A, W) {
 #' @export
 #'
 
-DGP_AL_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
+DGP_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
 
   # Covariates
   W1 = rnorm(n)
@@ -57,11 +57,11 @@ DGP_AL_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
   W = data.frame(W1, W2, W3, W4)
 
   u = runif(n)
-  Y = as.numeric(u<QAW_AL_bin(A,W))
+  Y = as.numeric(u<QAW_bin(A,W))
 
   # Blip function
-  QAW1 = QAW_AL_bin(A = 1, W)
-  QAW0 = QAW_AL_bin(A = 0, W)
+  QAW1 = QAW_bin(A = 1, W)
+  QAW0 = QAW_bin(A = 0, W)
   blip = QAW1 - QAW0
 
   # Treatment under rule
@@ -86,7 +86,7 @@ DGP_AL_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
   }
 
   # Outcome
-  Y_star = as.numeric(u<QAW_AL_bin(A_star,W))
+  Y_star = as.numeric(u<QAW_bin(A_star,W))
 
   # Data and target parameter
   O = data.frame(W, A, A_star, Y, Y_star)
@@ -109,8 +109,14 @@ DGP_AL_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
 
 
 
-#' @name QAW_smooth2
-#' @aliases QAW_smooth2
+
+
+
+
+
+
+#' @name QAW_cont
+#' @aliases QAW_cont
 #' @title Simulate with DB DGP
 #' @description Generate QAW according to DB DGP
 #'
@@ -123,10 +129,10 @@ DGP_AL_bin = function(n, dA = NULL, a = NULL, kappa = NULL){
 #'
 
 ###############################################
-#### DB smooth2 outcome #########################
+#### DB cont outcome #########################
 ###############################################
 # QAW
-QAW_smooth2 = function(A, W) {
+QAW_cont = function(A, W) {
 
   W1 = W$W1
   W2 = W$W2
@@ -137,8 +143,8 @@ QAW_smooth2 = function(A, W) {
 
 }
 
-#' @name DGP_smooth2
-#' @aliases DGP_smooth2
+#' @name DGP_cont
+#' @aliases DGP_cont
 #' @title Simulate with DB DGP
 #' @description Generate data according to DB DGP
 #'
@@ -152,7 +158,7 @@ QAW_smooth2 = function(A, W) {
 #' @export
 #'
 #'
-DGP_smooth2 = function(n, dA = NULL, a = NULL, kappa = NULL){
+DGP_cont = function(n, dA = NULL, a = NULL, kappa = NULL){
 
   # Covariates
   W1 <- runif(n,-4,4)
@@ -164,11 +170,11 @@ DGP_smooth2 = function(n, dA = NULL, a = NULL, kappa = NULL){
   W = data.frame(W1, W2, W3, W4)
 
   z = rnorm(n)
-  Y = QAW_smooth2(A,W)+z
+  Y = QAW_cont(A,W)+z
 
   # Blip function
-  QAW1 = QAW_smooth2(A = 1, W)
-  QAW0 = QAW_smooth2(A = 0, W)
+  QAW1 = QAW_cont(A = 1, W)
+  QAW0 = QAW_cont(A = 0, W)
   blip = QAW1 - QAW0
 
   # Treatment under rule
@@ -193,7 +199,7 @@ DGP_smooth2 = function(n, dA = NULL, a = NULL, kappa = NULL){
   }
 
   # Outcome
-  Y_star = QAW_smooth2(A_star,W)+z
+  Y_star = QAW_cont(A_star,W)+z
 
   # Data and target parameter
   O = data.frame(W, A, A_star, Y, Y_star)
@@ -212,4 +218,95 @@ DGP_smooth2 = function(n, dA = NULL, a = NULL, kappa = NULL){
 
 
 
+#' @name QAW_cont2
+#' @aliases QAW_cont2
+#' @title Simulate with DB DGP with influential variable
+#' @description Generate QAW according to DB DGP with influential variable
+#'
+#' @param W Data frame of observed baseline covariates
+#' @param A Vector of treatment
+#'
+#' @return
+#'
+#' @export
+#'
 
+###############################################
+#### DB cont outcome #########################
+###############################################
+# QAW
+QAW_cont2 = function(A, W) {
+
+  W1 = W$W1
+  W2 = W$W2
+  W3 = W$W3
+  W4 = W$W4
+
+  return(W1/10 - 0.3*A*W1^2 + 0.25*W2 + 0.5*A*W1 + 0.2*W4^2/5 - 0.1*W4 + 2*A - 0.01*A*abs(W1))
+
+}
+
+#' @name DGP_cont2
+#' @aliases DGP_cont2
+#' @title Simulate with DB DGP with influential variable
+#' @description Generate data according to DB DGP with influential variable
+#'
+#' @param n n
+#' @param dA rule type
+#' @param a static txt
+#' @param kappa for resource constraints
+#'
+#' @return
+#'
+#' @export
+#'
+#'
+DGP_cont2 = function(n, dA = NULL, a = NULL, kappa = NULL){
+
+  # Covariates
+  W1 <- runif(n,-4,4)
+  W2 <- runif(n,-4,4)
+  A <- rbinom(n, 1, 0.5)
+  W3 <- rnorm(n)
+  W4 <- rgamma(n, 2, 1)
+
+  W = data.frame(W1, W2, W3, W4)
+
+  z = rnorm(n)
+  Y = QAW_cont2(A,W)+z
+
+  # Blip function
+  QAW1 = QAW_cont2(A = 1, W)
+  QAW0 = QAW_cont2(A = 0, W)
+  blip = QAW1 - QAW0
+
+  # Treatment under rule
+  if (!is.null(dA) & !is.null(a)){
+    stop("Can only have dA or a")
+  } else if (is.null(a) & is.null(dA)) {
+    A_star = A
+  } else if (!is.null(a)){
+    A_star = a
+  } else if (dA == "simple dynamic") {
+    A_star = ifelse(W2 > 0, 1, 0)
+  } else if (dA == "ODTR"){
+    A_star = as.numeric(blip > 0)
+  } else if (dA == "ODTR-RC" & is.null(kappa)){
+    stop("If you have dA as ODTR-RC you must specify a kappa")
+  } else if (dA == "ODTR-RC"){
+    tau = seq(from = min(blip), to = max(blip), length.out = 500) # let tau vary from min blip to max blip
+    surv = sapply(tau, function(x) mean(blip > x)) #probability that the blip is greater than some varying tau
+    nu = min(tau[which(surv <= kappa)]) #the biggest tau such that the survival prob is <= kappa
+    tauP = max(c(nu, 0)) # max between nu and 0
+    A_star = as.numeric(blip > tauP)
+  }
+
+  # Outcome
+  Y_star = QAW_cont2(A_star,W)+z
+
+  # Data and target parameter
+  O = data.frame(W, A, A_star, Y, Y_star)
+
+  return(O)
+
+}
