713b63490d03bd7fb1309f877808b158faacbfc6
lmmontoya <lmontoya@berkeley.edu>
2019-11-21 20:51:07 -0500

    add DGP cont3

--- R/1DGPfunctions.R
+++ R/1DGPfunctions.R
@@ -422,3 +422,105 @@ DGP_bin2 = function(n, dA = NULL, a = NULL, kappa = NULL){
 
 
 
+
+
+
+
+
+
+
+
+#' @name QAW_cont3
+#' @aliases QAW_cont3
+#' @title Simulate with DB DGP with simple influential variable
+#' @description Generate QAW according to DB DGP with simple influential variable
+#'
+#' @param W Data frame of observed baseline covariates
+#' @param A Vector of treatment
+#'
+#' @return
+#'
+#' @export
+#'
+
+###############################################
+#### DB cont outcome #########################
+###############################################
+# QAW
+QAW_cont3 = function(A, W) {
+
+  W1 = W$W1
+  W2 = W$W2
+  W3 = W$W3
+  W4 = W$W4
+
+  return(A + W1 + A*W1)
+
+}
+
+#' @name DGP_cont3
+#' @aliases DGP_cont3
+#' @title Simulate with DB DGP with simple influential variable
+#' @description Generate data according to DB DGP with simple influential variable
+#'
+#' @param n n
+#' @param dA rule type
+#' @param a static txt
+#' @param kappa for resource constraints
+#'
+#' @return
+#'
+#' @export
+#'
+#'
+DGP_cont3 = function(n, dA = NULL, a = NULL, kappa = NULL){
+
+  # Covariates
+  W1 <- runif(n,-4,4)
+  W2 <- runif(n,-4,4)
+  A <- rbinom(n, 1, 0.5)
+  W3 <- rnorm(n)
+  W4 <- rgamma(n, 2, 1)
+
+  W = data.frame(W1, W2, W3, W4)
+
+  z = rnorm(n)
+  Y = QAW_cont3(A,W)+z
+
+  # Blip function
+  QAW1 = QAW_cont3(A = 1, W)
+  QAW0 = QAW_cont3(A = 0, W)
+  blip = QAW1 - QAW0
+
+  # Treatment under rule
+  if (!is.null(dA) & !is.null(a)){
+    stop("Can only have dA or a")
+  } else if (is.null(a) & is.null(dA)) {
+    A_star = A
+  } else if (!is.null(a)){
+    A_star = a
+  } else if (dA == "simple dynamic") {
+    A_star = ifelse(W2 > 0, 1, 0)
+  } else if (dA == "ODTR"){
+    A_star = as.numeric(blip > 0)
+  } else if (dA == "ODTR-RC" & is.null(kappa)){
+    stop("If you have dA as ODTR-RC you must specify a kappa")
+  } else if (dA == "ODTR-RC"){
+    tau = seq(from = min(blip), to = max(blip), length.out = 500) # let tau vary from min blip to max blip
+    surv = sapply(tau, function(x) mean(blip > x)) #probability that the blip is greater than some varying tau
+    nu = min(tau[which(surv <= kappa)]) #the biggest tau such that the survival prob is <= kappa
+    tauP = max(c(nu, 0)) # max between nu and 0
+    A_star = as.numeric(blip > tauP)
+  }
+
+  # Outcome
+  Y_star = QAW_cont3(A_star,W)+z
+
+  # Data and target parameter
+  O = data.frame(W, A, A_star, Y, Y_star)
+
+  return(O)
+
+}
+
+
