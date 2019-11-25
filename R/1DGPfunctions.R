f56fb63c1ad188253c555dedcaa9c97e5370035b
lmmontoya <lmontoya@berkeley.edu>
2019-11-25 15:49:42 -0500

    add bin3

--- R/1DGPfunctions.R
+++ R/1DGPfunctions.R
@@ -381,11 +381,11 @@ DGP_bin2 = function(n, dA = NULL, a = NULL, kappa = NULL){
   W = data.frame(W1, W2, W3, W4)
 
   u = runif(n)
-  Y = as.numeric(u<QAW_bin(A,W))
+  Y = as.numeric(u<QAW_bin2(A,W))
 
   # Blip function
-  QAW1 = QAW_bin(A = 1, W)
-  QAW0 = QAW_bin(A = 0, W)
+  QAW1 = QAW_bin2(A = 1, W)
+  QAW0 = QAW_bin2(A = 0, W)
   blip = QAW1 - QAW0
 
   # Treatment under rule
@@ -410,7 +410,7 @@ DGP_bin2 = function(n, dA = NULL, a = NULL, kappa = NULL){
   }
 
   # Outcome
-  Y_star = as.numeric(u<QAW_bin(A_star,W))
+  Y_star = as.numeric(u<QAW_bin2(A_star,W))
 
   # Data and target parameter
   O = data.frame(W, A, A_star, Y, Y_star)
@@ -522,5 +522,130 @@ DGP_cont3 = function(n, dA = NULL, a = NULL, kappa = NULL){
   return(O)
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' @name QAW_bin3
+#' @aliases QAW_bin3
+#' @title Simulate with AL bin DGP with influential variable
+#' @description Generate QAW according to AL bin DGP with influential variable
+#'
+#' @param W Data frame of observed baseline covariates
+#' @param A Vector of treatment
+#'
+#' @return
+#'
+#' @export
+#'
+
+
+###############################################
+### AL DGP binary outcome #####################
+###############################################
+# QAW3
+QAW_bin3 = function(A, W) {
+
+  W1 = W$W1
+  W2 = W$W2
+  W3 = W$W3
+  W4 = W$W4
+
+  return(plogis(W1 + 0.1*A + W1*A))
+
+}
+
+
+
+#' @name DGP_bin3
+#' @aliases DGP_bin3
+#' @title Simulate with AL bin DGP with influential variable
+#' @description Generate data according to AL bin DGP with influential variable
+#'
+#' @param n n
+#' @param dA rule type
+#' @param a static txt
+#' @param kappa for resource constraints
+#'
+#' @return
+#'
+#' @export
+#'
+
+DGP_bin3 = function(n, dA = NULL, a = NULL, kappa = NULL){
+
+  # Covariates
+  W1 = rnorm(n)
+  W2 = rnorm(n)
+  W3 = rnorm(n)
+  W4 = rnorm(n)
+
+  A = rbinom(n, size = 1, prob = 0.5)
+
+  W = data.frame(W1, W2, W3, W4)
+
+  u = runif(n)
+  Y = as.numeric(u<QAW_bin3(A,W))
+
+  # Blip function
+  QAW1 = QAW_bin3(A = 1, W)
+  QAW0 = QAW_bin3(A = 0, W)
+  blip = QAW1 - QAW0
+
+  # Treatment under rule
+  if (!is.null(dA) & !is.null(a)){
+    stop("Can only have dA or a")
+  } else if (is.null(a) & is.null(dA)) {
+    A_star = A
+  } else if (!is.null(a)){
+    A_star = a
+  } else if (dA == "simple dynamic") {
+    A_star = ifelse(W2 > 0, 1, 0)
+  } else if (dA == "ODTR"){
+    A_star = as.numeric(blip > 0)
+  } else if (dA == "ODTR-RC" & is.null(kappa)){
+    stop("If you have dA as ODTR-RC you must specify a kappa")
+  } else if (dA == "ODTR-RC"){
+    tau = seq(from = min(blip), to = max(blip), length.out = 500) # let tau vary from min blip to max blip
+    surv = sapply(tau, function(x) mean(blip > x)) #probability that the blip is greater than some varying tau
+    nu = min(tau[which(surv <= kappa)]) #the biggest tau such that the survival prob is <= kappa
+    tauP = max(c(nu, 0)) # max between nu and 0
+    A_star = as.numeric(blip > tauP)
+  }
+
+  # Outcome
+  Y_star = as.numeric(u<QAW_bin3(A_star,W))
+
+  # Data and target parameter
+  O = data.frame(W, A, A_star, Y, Y_star)
+
+  return(O)
+
+}
+
+
 
 
